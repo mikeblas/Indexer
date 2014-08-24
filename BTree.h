df145e71a7fe2f9595f2130b7612df4b4d9b683f
Mike Blaszczak <mikeblas@gmail.com>
2014-08-24 10:08:55 -0700

    Some more structure

--- BTree.h
+++ BTree.h
@@ -1,13 +1,25 @@
 
 #include <unordered_map>
+
 #include "BTreeNode.h"
+
+#pragma once
 
 class BTree {
 
-	std::unordered_map<int, BTreeNode> m_pageCache;
+	std::unordered_map<int, BTreeNode*> m_pageCache;
+
+	BTreeNode* m_pRoot;
 
 public:
 	BTree() {
-		m_pageCache = new std::unordered_map<int, BTreeNode>();
+		m_pRoot = NULL;
+	}
+
+	void Insert( __int64 nNewKey ) { 
+	}
+
+	bool HasKey( __int64 nNewKey ) {
+		return m_pRoot->HasKey( nNewKey );
 	}
 };
